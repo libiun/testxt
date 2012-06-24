fb0fcd3fece299ff49865c79849299f6c940c20e
libiun <libiun@gmail.com>
2012-06-24 23:28:52 +0800

    baidu map api test

--- js/main.js
+++ js/main.js
@@ -1,3 +1,17 @@
-$(function(){
-	//window.alert("js is in");
-});
+function baidu_map(){
+	var map=new BMap.Map("map_canvas");
+	var point=new BMap.Point(116.404, 39.915);
+	var marker=new BMap.Marker(point);
+	marker.addEventListener("click",function(){
+		alert("you clicked marker");
+	});
+	
+	map.centerAndZoom(point, 15);
+	map.enableScrollWheelZoom();
+	map.addControl(new BMap.NavigationControl());
+	map.addControl(new BMap.ScaleControl());
+	map.addControl(new BMap.OverviewMapControl());
+	map.addControl(new BMap.MapTypeControl());
+	map.setCurrentCity("北京");
+	map.addOverlay(marker);
+}
